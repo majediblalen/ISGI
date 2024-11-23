ae6d7e105cdbe8a092f9f824e601648fcc0035aa
majediblalen <majdiblalen.1@gmail.com>
2024-11-23 10:12:19 +0100

    function num 1

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function check(e){
-    e.preventDefault()
-    for (let index = 1; index < word.length; index++) {
-        let id = word[index]
-        var inp_value = document.getElementById(id)
-        if (inp_value.value==id){
-            console.log("true")
-            inp_value.value = id
-    }else{
-        document.getElementById(id).value =""
-    }
-}
+var inp_value = document.getElementById('input');
+var form = document.getElementById('form');
+const mots = ["pomme", "banane", "orange", "mangue", "ananas", "fraise"];
+
+var word = mots.sort(() => Math.random() - 0.5)[0];
+var sliced_word = word[0];
+inp_value.value = sliced_word;
+inp_value.id = sliced_word;
+
+for (let index = 1; index < word.length; index++) {
+    let id = word[index];
+    var inp_created = document.createElement('input');
+    inp_created.id = id;
+    form.append(inp_created)
 }
